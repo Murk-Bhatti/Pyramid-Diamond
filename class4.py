e5c9d49b9c41c6b99b84959cc5b59f0931ab2c2b
Murk-Bhatti <165047752+Murk-Bhatti@users.noreply.github.com>
2025-04-12 19:58:31 +0500

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,17 @@
+#Row = 10
+
+# for i in range(1, Row + 1):
+#     print(' ' * (Row - i), end='')
+#     print('*' * (2 * i - 1))
+
+
+#Daimond
+rows = 5
+
+#Top
+for i in range(1, rows + 1):
+    print(' ' * (rows - i) + '*' * (2 * i - 1))
+
+# Bottom
+for i in range(rows - 1, 0, -1):
+    print(' ' * (rows - i) + '*' * (2 * i - 1))
